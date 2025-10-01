90ee515478a51321a04b2fa6086cb3628d8c27e0
erkamerf <erkamerf@yahoo.com>
2025-09-30 22:06:42 -0500

    changes to cure sets

--- Harissa_Sch_Gear.lua
+++ Harissa_Sch_Gear.lua
@@ -112,20 +112,14 @@ function init_gear_sets()
 		body="Zendik Robe",hands="Gende. Gages +1",ring1="Kishar Ring",ring2="Prolix Ring",
 		back="Swith Cape +1",waist="Witful Belt",legs="Psycloth Lappas",feet="Regal Pumps +1"}
 		
-    sets.midcast.Cure = {main="Serenity",sub="Curatio Grip",ammo="Hasty Pinion +1",
-        head="Gende. Caubeen +1",neck="Incanter's Torque",ear1="Meili Earring",ear2="Malignance Earring",
-        body="Kaykaus Bliaut",hands="Kaykaus Cuffs",ring1="Janniston Ring",ring2="Lebeche Ring",
-        back="Tempered Cape +1",waist="Luminary Sash",legs="Chironic Hose",feet="Kaykaus Boots"}
+    sets.midcast.Cure = {main="Daybreak",sub="Sors Shield",ammo="Staunch Tathlum +1",
+        head="Vanya Hood",neck="Deviant Necklace",ear1="Magnetic Earring",ear2="Novia Earring",
+        body="Kaykaus Bliaut",hands="Peda. bracers +2",ring1="Naji's Loop",ring2="Metamor. Ring +1",
+        back="Twilight Cape",waist="Porous Rope",legs="Vanya Slops",feet="Kaykaus Boots"}
 		
-    sets.midcast.LightWeatherCure = {main="Chatoyant Staff",sub="Curatio Grip",ammo="Hasty Pinion +1",
-        head="Gende. Caubeen +1",neck="Incanter's Torque",ear1="Meili Earring",ear2="Malignance Earring",
-        body="Kaykaus Bliaut",hands="Kaykaus Cuffs",ring1="Janniston Ring",ring2="Lebeche Ring",
-        back="Twilight Cape",waist="Hachirin-no-Obi",legs="Chironic Hose",feet="Kaykaus Boots"}
+    sets.midcast.LightWeatherCure = set_combine(sets.midcast.Cure, {main="Chatoyant Staff",sub="Khonsu",ring2="Lebeche Ring",waist="Hachirin-no-Obi"})
 		
-    sets.midcast.LightDayCure = {main="Serenity",sub="Curatio Grip",ammo="Hasty Pinion +1",
-        head="Gende. Caubeen +1",neck="Incanter's Torque",ear1="Meili Earring",ear2="Malignance Earring",
-        body="Kaykaus Bliaut",hands="Kaykaus Cuffs",ring1="Janniston Ring",ring2="Lebeche Ring",
-        back="Twilight Cape",waist="Hachirin-no-Obi",legs="Chironic Hose",feet="Kaykaus Boots"}
+    sets.midcast.LightDayCure = set_combine(sets.midcast.Cure, {waist="Hachirin-no-Obi",})
 
     sets.midcast.Curaga = sets.midcast.Cure
 
